0a910533375675725c8247c7a9e31c62a3897f41
Andre Suarez Schou <andre_suarez_schou@hotmail.com>
2022-03-27 01:12:55 +0100

    efefs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-let i = 2;
-let x = 10;
-if (i<x){
-    console.log("x is bigger than i");
+
+let index = 1;
+
+let pages = Array.from(document.getElementsByClassName("page"));
+
+pages[index - 1].style.display = "block";
+
+const carousel = (e) => {
+let posX = e.clientX;
+let viewportCenter = window.innerWidth / 2;
+pages.forEach(page => page.style.display = "none");
+if (posX > viewportCenter) {
+index++;
+if (index > pages.length) {
+index = 1;
 }
+} else {
+index--;
+if (index < 1) {
+index = pages.length;
+}
+}
+
+pages[index - 1].style.display = "block";
+}
+
+document.addEventListener("click", carousel);
